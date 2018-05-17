0f36ea1f9ebd5dc24f206de43727c3acf1a284c0
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-17 16:24:54 +0700

    add rollup license plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,34 @@
-import typescript from 'rollup-plugin-typescript2';
-import babel from 'rollup-plugin-babel';
+import path from "path";
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import license from "rollup-plugin-license";
 
 export default {
-    input: './lib/index.ts',
+    input: "./lib/index.ts",
     output: {
-        file: './dist/index.js',
-        format: 'cjs',
+        file: "./dist/index.js",
+        format: "cjs",
     },
     plugins: [
         typescript(),
         babel({
-            exclude: 'node_modules/**',
+            exclude: "node_modules/**",
             presets: [
                 [
-                    'env',
+                    "env",
                     {
                         modules: false,
                     },
                 ],
             ],
-            plugins: [
-                'external-helpers',
-            ],
+            plugins: ["external-helpers"],
+        }),
+        license({
+            sourceMap: true,
+            banner: {
+                file: path.join(__dirname, "license"),
+                encoding: "utf-8", // Default is utf-8
+            },
         }),
     ],
 };
