37fdea8af4b94c9b8ea6338b3075eaa8a287dff9
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-17 23:21:36 +0700

    generate types on compile and some formatting

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,33 @@ import babel from "rollup-plugin-babel";
 import license from "rollup-plugin-license";
 
 export default {
-    input: "./lib/index.ts",
-    output: {
-        file: "./dist/index.js",
-        format: "cjs",
-    },
-    plugins: [
-        typescript(),
-        babel({
-            exclude: "node_modules/**",
-            presets: [
-                [
-                    "env",
-                    {
-                        modules: false,
-                    },
-                ],
-            ],
-            plugins: ["external-helpers"],
-        }),
-        license({
-            sourceMap: true,
-            banner: {
-                file: path.join(__dirname, "license"),
-                encoding: "utf-8", // Default is utf-8
-            },
-        }),
-    ],
+  input: "./lib/index.ts",
+  output: {
+    file: "./dist/index.js",
+    format: "cjs",
+  },
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      presets: [
+        [
+          "env",
+          {
+            modules: false,
+          },
+        ],
+      ],
+      plugins: ["external-helpers"],
+    }),
+    license({
+      sourceMap: true,
+      banner: {
+        file: path.join(__dirname, "license"),
+        encoding: "utf-8", // Default is utf-8
+      },
+    }),
+  ],
 };
