c5ad585c223af0b062f3193298e0f90023a5e3e5
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-19 17:47:00 +0700

    export some useable class/interface

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 import { RouteCollection } from "./RouteCollection";
 
 export default RouteCollection;
+
+export { RouteCollectionConfig, RouteGroupConfig } from "./RouteCollection";
+export { RouteBuilderConfig, RouteGuard, RouteGuardHanldeResult } from "./Route";
