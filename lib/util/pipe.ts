b3105e743eff7b74096cdb74b9df20132dae97a7
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-22 07:04:17 +0700

    add tapAsync and pipeAsync util

--- lib/util/pipe.ts
+++ lib/util/pipe.ts
@@ -3,3 +3,13 @@ export function pipe<T>(value: T, ...pipes: ((value: T) => T)[]): T {
     return pipe(val);
   }, value);
 }
+
+export async function pipeAsync<T>(value: T, ...pipes: ((value: T) => Promise<T>)[]): Promise<T> {
+  let result = value;
+
+  for (const pipe of pipes) {
+    result = await pipe(result);
+  }
+
+  return result;
+}
--- lib/util/tap.ts
+++ lib/util/tap.ts
@@ -2,3 +2,11 @@ export function tap<T>(value: T, ...fns: ((value: T) => void)[]): T {
   fns.forEach(fn => fn(value));
   return value;
 }
+
+export async function tapAsync<T>(value: T, ...fns: ((value: T) => Promise<void>)[]): Promise<T> {
+  for (const fn of fns) {
+    await fn(value);
+  }
+
+  return value;
+}
