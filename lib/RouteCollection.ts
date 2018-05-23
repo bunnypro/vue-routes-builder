faf739b3e2b99f3e1ae51a4215963b3aafb2d789
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-24 05:05:21 +0700

    move WrappedRouteCollection to RouteCollection file

--- lib/RouteCollection.ts
+++ lib/RouteCollection.ts
@@ -1,7 +1,6 @@
 import { Component, Dictionary, RouteConfig } from "vue-router/types/router";
 import { tap, flatten } from "./util";
 import { RouteGuardType } from "./RouteGuard";
-import { WrappedRouteCollection } from "./WrappedRouteCollection";
 import { Route, RouteBuilderConfig } from "./Route";
 
 export interface RouteGroupConfig {
@@ -69,5 +68,33 @@ export class RouteChildren extends RouteCollection {
     const rPath = super.resolveRoutePath(path);
 
     return rPath === "/" ? "/" : rPath.replace(/^\/+/g, "");
+  }
+}
+
+export class WrappedRouteCollection implements IRouteCollection {
+  private readonly _prefix: string;
+  private readonly _guards: RouteGuardType[];
+  private readonly _routes: IRouteCollection;
+
+  constructor(config: RouteGroupConfig, routes: IRouteCollection) {
+    this._prefix = config.prefix || "/";
+    this._routes = routes;
+    this._guards = config.guards || [];
+  }
+
+  get count(): number {
+    return this._routes.count;
+  }
+
+  guards(): RouteGuardType[] {
+    return [...this._routes.guards(), ...this._guards];
+  }
+
+  build(...parents: IRouteCollection[]): RouteConfig[] {
+    return this._routes.build(...parents, this);
+  }
+
+  resolveRoutePath(path: string): string {
+    return `${this._prefix}/${path}`;
   }
 }
