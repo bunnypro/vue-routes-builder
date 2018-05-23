d41f741c98b878970722923a775a14ead154c5bd
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-24 05:41:53 +0700

    remove unnecessary exposed api

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,3 @@
 import { RouteCollection } from "./RouteCollection";
-
 export default RouteCollection;
-
-export { RouteBuilderConfig } from "./Route";
-export { RouteGroupConfig } from "./RouteCollection";
-export { RouteGuard, RouteGuardHanldeResult } from "./RouteGuard";
+export { RouteGuard } from "./RouteGuard";
