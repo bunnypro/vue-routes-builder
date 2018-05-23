782f19e67a95aa5c5644ea2f109a0712bb7e66f3
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-05-24 05:02:31 +0700

    using implicit array

--- lib/util/flatMap.ts
+++ lib/util/flatMap.ts
@@ -1,6 +1,6 @@
 import { RecursiveArray } from "./common";
 import { flatten } from "./flatten";
 
-export function flatMap<T, TResult>(items: Array<T>, fn: (item: T) => RecursiveArray<TResult>): TResult[] {
+export function flatMap<T, TResult>(items: T[], fn: (item: T) => RecursiveArray<TResult>): TResult[] {
   return flatten(items.map(fn));
 }
