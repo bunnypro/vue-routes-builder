c7412dd26ddbe015b6ec12fdb0806bec40283e9c
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-06-08 15:00:45 +0700

    improve RouteGuard return typehint

--- lib/RouteGuard.ts
+++ lib/RouteGuard.ts
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import { Route, RawLocation } from "vue-router/types/router";
 
-export type RouteGuardResult = RawLocation | boolean | void;
+type PossibleRouteGuardResult = RawLocation | boolean | void;
+type AsyncPossibleRouteGuardResult = Promise<PossibleRouteGuardResult>;
+
+export type RouteGuardResult = PossibleRouteGuardResult | AsyncPossibleRouteGuardResult;
 
 export abstract class RouteGuard {
-  abstract handle(from: Route, to: Route): RouteGuardResult | Promise<RouteGuardResult>;
+  abstract handle(from: Route, to: Route): RouteGuardResult;
 }
 
 export type RouteGuardType = RouteGuard | ((to: Route, from: Route) => RouteGuardResult);
