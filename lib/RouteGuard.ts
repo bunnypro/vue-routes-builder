8e20d4b4dea93cfdc32b479cf7b843cbe7aa2bcd
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-06-08 13:59:43 +0700

    remove callback return typehint for RouteGuard

--- lib/RouteGuard.ts
+++ lib/RouteGuard.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import { Route, RawLocation } from "vue-router/types/router";
 
-export type RouteGuardHanldeResult = RawLocation | boolean | ((vm: Vue) => any) | void;
+export type RouteGuardHanldeResult = RawLocation | boolean | void;
 
 export abstract class RouteGuard {
   abstract handle(from: Route, to: Route): RouteGuardHanldeResult | Promise<RouteGuardHanldeResult>;
